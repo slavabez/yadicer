319f3b02cefd368b123020cb5d7005dba25f1962
Slava Bezgachev <bezgachev@gmail.com>
2018-09-10 20:56:30 +0600

    Added the very simple roller, now to add simple tests

---
+++ src/Roller.js
@@ -0,0 +1,37 @@
+/**
+ * Returns an object that represents the dice roll
+ * @param input - Expects a standard roll notation XdY, where X is number of dice and y is sides per die
+ */
+export default function roll(input) {
+  return new Promise((resolve, reject) => {
+    // Make sure it's valid notation
+    const valid = input.match(/\d+[d]\d+/);
+    if (!valid)
+      reject(
+        "Not a valid roll notation, needs to be integer, d, integer, e.g. 4d20"
+      );
+    // Delimiter by d to get number of rolls, then number of die size
+    const elements = input.split("d");
+    const rolls = parseInt(elements[0]);
+    if (isNaN(rolls) || rolls === 0) reject("Invalid number of rolls");
+    const sides = parseInt(elements[1]);
+    if (isNaN(sides) || sides === 0) reject("Invalid number of sides");
+
+    // Results will be an object with total count and an array of individual rolls
+    const results = {};
+    results.rolls = [];
+    results.total = 0;
+
+    for (let i = 0; i < rolls; i++){
+      results.rolls.push({
+        order: i,
+        sides: sides,
+        result: Math.ceil(Math.random() * sides)
+      });
+      // Keep a total count
+      results.total += results.roll[i].result
+    }
+
+    resolve(results);
+  });
+}
